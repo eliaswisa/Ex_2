5256c083c0268ea519d4fdd5ecd23d26c598c232
eliaswisa <elia030688@gmail.com>
2020-12-01 00:33:18 +0200

    starting graph

--- src/api/DWGraph_DS.java
+++ src/api/DWGraph_DS.java
@@ -8,22 +8,36 @@ public class DWGraph_DS implements directed_weighted_graph {
     private HashMap<Integer, node_data> nodes;
     private HashMap<Integer, HashMap<Integer, edge_data>> edges;
     private int edge_counter = 0;
+    private int nodes_counter = 0;
     private int MC = 0;
 
 
     @Override
     public node_data getNode(int key) {
-        return null;
+        if (this.nodes.get(key) == null) {
+            return null;
+        }
+        return this.nodes.get(key);
     }
 
     @Override
     public edge_data getEdge(int src, int dest) {
+        if (this.nodes.get(src) == null || this.nodes.get(dest) == null) {
+            return null;
+        }
+        if (this.edges.get(src)!=null) {
+            if (this.edges.get(src).get(dest) != null) {
+                return this.edges.get(src).get(dest);
+            }
+        }
         return null;
     }
 
     @Override
     public void addNode(node_data n) {
-
+        nodes.put(n.getKey(), (NodeData) n);
+        MC++;
+        nodes_counter++;
     }
 
     @Override
