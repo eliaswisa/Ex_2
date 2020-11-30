84778b78056c77bff512997d3593bbc4a2aebe8b
eliaswisa <elia030688@gmail.com>
2020-11-30 20:01:16 +0200

    finish node

---
+++ src/api/GeoLocation.java
@@ -0,0 +1,38 @@
+package api;
+
+public class GeoLocation implements geo_location {
+   private double x;
+    private double y;
+    private double z;
+    @Override
+    public double x() {
+        return this.x;
+    }
+
+    @Override
+    public double y() {
+        return this.y;
+    }
+
+    @Override
+    public double z() {
+        return this.z;
+    }
+
+    @Override
+    public double distance(geo_location g) {
+        double d;
+        return Math.sqrt(Math.pow(g.x()-this.x,2)+Math.pow(g.y()-this.y,2)+Math.pow(g.z()-this.z,2));
+    }
+
+    /**
+     * copy constructor
+     * @param geo
+     */
+    public GeoLocation (geo_location geo)
+    {
+        this.x=geo.x();
+        this.y=geo.y();
+        this.z=geo.z();
+    }
+}
--- src/api/NodeData.java
+++ src/api/NodeData.java
@@ -1,48 +1,76 @@
 package api;
 
+import gameClient.util.Point3D;
+
 public class NodeData implements node_data {
+
+    private int key;
+    private double weight;
+    private int tag;
+    private String info;
+    public static int id = 1;
+    private geo_location location;
+
+    public NodeData() {
+        this.location = Point3D.ORIGIN;
+        this.key = id;
+        this.weight = Double.MAX_VALUE;
+        this.tag = 0;
+        this.info = "";
+        this.id++;
+    }
+
+    public NodeData(GeoLocation location) {
+        this.location = location;
+        this.key = id;
+        this.id++;
+        this.weight = Double.MAX_VALUE;
+        this.tag = 0;
+        this.info = "";
+    }
+
     @Override
     public int getKey() {
-        return 0;
+        return this.key;
     }
 
     @Override
     public geo_location getLocation() {
-        return null;
+        return this.location;
     }
 
     @Override
     public void setLocation(geo_location p) {
-
+        this.location = new Point3D(p.x(), p.y(), p.z());
     }
 
     @Override
     public double getWeight() {
-        return 0;
+        return this.weight;
     }
 
     @Override
     public void setWeight(double w) {
-
+        this.weight = w;
     }
 
     @Override
     public String getInfo() {
-        return null;
+        return this.info;
     }
 
     @Override
     public void setInfo(String s) {
-
+        this.info = s;
     }
 
     @Override
     public int getTag() {
-        return 0;
+        return this.tag;
     }
 
     @Override
     public void setTag(int t) {
-
+        tag = t;
     }
 }
